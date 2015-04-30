478f66285c2b66322c44e3c0cb30afc6c3871aa4
Floriment <klinakuf@gmail.com>
2015-04-30 15:57:34 +0200

    the first test: exchanging messages

--- test01/src/com/example/test01/UMundoPingPongActivity.java
+++ test01/src/com/example/test01/UMundoPingPongActivity.java
@@ -19,19 +19,20 @@ import android.widget.TextView;
 /**
  * Getting it to run:
  * 
- * 1. Replace the umundo.jar in the libs folder by the one from the intaller and 
- * add it to the classpath. Make sure to take the umundo.jar for Android, as you 
- * are not allowed to have JNI code within and the desktop umundo.jar includes all 
- * supported JNI libraries.
+ * 1. Replace the umundo.jar in the libs folder by the one from the intaller and
+ * add it to the classpath. Make sure to take the umundo.jar for Android, as you
+ * are not allowed to have JNI code within and the desktop umundo.jar includes
+ * all supported JNI libraries.
  * 
- * 2. Replace the JNI library libumundoNativeJava.so (or the debug variant) into libs/armeabi/
+ * 2. Replace the JNI library libumundoNativeJava.so (or the debug variant) into
+ * libs/armeabi/
  * 
  * 3. Make sure System.loadLibrary() loads the correct variant.
  * 
- * 4. Make sure you have set the correct permissions:
- * 	  <uses-permission android:name="android.permission.INTERNET"/>
- *		<uses-permission android:name="android.permission.CHANGE_WIFI_MULTICAST_STATE"/>
- *   	<uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>
+ * 4. Make sure you have set the correct permissions: <uses-permission
+ * android:name="android.permission.INTERNET"/> <uses-permission
+ * android:name="android.permission.CHANGE_WIFI_MULTICAST_STATE"/>
+ * <uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>
  * 
  * @author sradomski
  *
@@ -50,8 +51,12 @@ public class UMundoPingPongActivity extends Activity {
 		@Override
 		public void run() {
 			String message = "This is foo from android";
+
 			while (testPublishing != null) {
-				fooPub.send(message.getBytes());
+				Message msg = new Message();
+				msg.putMeta("position", "34");
+				msg.setData(message.getBytes());
+				fooPub.send(msg);
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -68,7 +73,7 @@ public class UMundoPingPongActivity extends Activity {
 	}
 
 	public class TestReceiver extends Receiver {
-		public void receive(Message msg) {
+		public void receive(final Message msg) {
 
 			for (String key : msg.getMeta().keySet()) {
 				Log.i("umundo", key + ": " + msg.getMeta(key));
@@ -76,7 +81,9 @@ public class UMundoPingPongActivity extends Activity {
 			UMundoPingPongActivity.this.runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
-						tv.setText(tv.getText() + "i");
+					tv.setText(tv.getText() + "Message: "
+							+ new String(msg.getData())
+							+ msg.getMeta("position"));
 				}
 			});
 		}
@@ -100,21 +107,21 @@ public class UMundoPingPongActivity extends Activity {
 			Log.v("android-umundo", "Cannot get WifiManager");
 		}
 
-//		System.loadLibrary("umundoNativeJava");
+		// System.loadLibrary("umundoNativeJava");
 		System.loadLibrary("umundoNativeJava_d");
 
 		disc = new Discovery(DiscoveryType.MDNS);
-    
+
 		node = new Node();
 		disc.add(node);
 
 		fooPub = new Publisher("pingpong");
 		node.addPublisher(fooPub);
-    
+
 		fooSub = new Subscriber("pingpong");
 		fooSub.setReceiver(new TestReceiver());
 		node.addSubscriber(fooSub);
-    
+
 		testPublishing = new Thread(new TestPublishing());
 		testPublishing.start();
 	}
