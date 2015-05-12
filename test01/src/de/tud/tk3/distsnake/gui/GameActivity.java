5da67c4e4caaf248f9aadce1bc8a582779d270c8
Satia Herfert <satiaherfert@gmx.de>
2015-05-12 16:05:51 +0200

    Rectangular canvas. Painting circles instead of paths.

--- test01/src/de/tud/tk3/distsnake/gui/GameActivity.java
+++ test01/src/de/tud/tk3/distsnake/gui/GameActivity.java
@@ -1,10 +1,12 @@
 package de.tud.tk3.distsnake.gui;
 
+import de.tud.tk3.distsnake.GameStatus.Coordinates;
+import de.tud.tk3.distsnake.GameStatus.GameState;
+import de.tud.tk3.distsnake.GameStatus.GameState.Orientation;
 import de.tud.tk3.distsnake.R;
 import de.tud.tk3.distsnake.R.id;
 import de.tud.tk3.distsnake.R.layout;
 import de.tud.tk3.distsnake.R.menu;
-
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -14,8 +16,12 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 
 public class GameActivity extends Activity {
@@ -24,31 +30,46 @@ public class GameActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
-		Paint paint = new Paint();
-		paint.setColor(Color.parseColor("#CD5C5C"));
-		paint.setStrokeWidth(10);
-		paint.setStyle(Paint.Style.STROKE);
 
-		float[] intervals = new float[] { 30.0f, 10.0f };
-		float phase = 0;
-
-		DashPathEffect dashPathEffect = new DashPathEffect(intervals, phase);
-
-		paint.setPathEffect(dashPathEffect);
-
-		Path path = new Path();
-
-		path.moveTo(50, 50);
-		path.lineTo(50, 500);
-		path.lineTo(200, 500);
-		path.lineTo(200, 300);
-		path.lineTo(350, 300);
-		LinearLayout ll = (LinearLayout) findViewById(R.id.canvas);
-		Bitmap bg = Bitmap.createBitmap(480, 800, Bitmap.Config.ARGB_8888);
+		// Create Bitmap and Canvas
+		DisplayMetrics metrics = getResources().getDisplayMetrics();
+		int width = metrics.widthPixels;
+		Bitmap bg = Bitmap.createBitmap(metrics, width, width,
+				Bitmap.Config.ARGB_8888);
 		Canvas canvas = new Canvas(bg);
-		canvas.drawPath(path, paint);
 
-		ll.setBackground(new BitmapDrawable(bg));
+		// Integrate canvas into layout
+		LinearLayout ll = (LinearLayout) findViewById(R.id.canvas);
+		ll.setBackground(new BitmapDrawable(getResources(), bg));
+		ll.setLayoutParams(new GridLayout.LayoutParams(new LayoutParams(width,
+				width)));
+
+		// Paint for the backgroud
+		Paint bgPaint = new Paint();
+		bgPaint.setColor(Color.parseColor("#DADADA"));
+		bgPaint.setStyle(Paint.Style.FILL);
+		canvas.drawRect(0, 0, width, width, bgPaint);
+
+		// Mockup data TODO
+		Coordinates s0 = Coordinates.newBuilder().setX(3).setY(5).build();
+		Coordinates s1 = Coordinates.newBuilder().setX(3).setY(6).build();
+		Coordinates s2 = Coordinates.newBuilder().setX(4).setY(6).build();
+		Coordinates goalCoordinates = Coordinates.newBuilder().setX(10)
+				.setY(12).build();
+		GameState gameState = GameState.newBuilder().addSnake(s0).addSnake(s1)
+				.addSnake(s2).setCurrentPlayer("Blah").setGoal(goalCoordinates)
+				.setOrient(Orientation.NORTH).setRemainSteps(1).build();
+		
+		// Paint for the backgroud
+		Paint snakePaint = new Paint();
+		snakePaint.setColor(Color.parseColor("#000000"));
+		snakePaint.setStrokeWidth(10);
+		snakePaint.setStyle(Paint.Style.FILL);
+
+		canvas.drawRect(0, 0, width, width, bgPaint);
+		
+		// Draw snake
+		canvas.drawCircle(30, 30, 5, snakePaint);
 	}
 
 	@Override
