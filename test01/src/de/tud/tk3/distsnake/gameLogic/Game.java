091b326f3e99cc690fab77b8a50193a790055406
Satia Herfert <satiaherfert@gmx.de>
2015-05-14 17:41:56 +0200

    Implemented the methods in Game chainging the orientation

--- test01/src/de/tud/tk3/distsnake/gameLogic/Game.java
+++ test01/src/de/tud/tk3/distsnake/gameLogic/Game.java
@@ -4,18 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.tud.tk3.distsnake.GameStatus.GameState;
+import de.tud.tk3.distsnake.GameStatus.GameState.Orientation;
 
 public class Game {
-	
+
 	// Singleton stuff
 	private static Game instance;
+
 	public static synchronized Game getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new Game();
 		}
 		return instance;
 	}
-	
+
 	/**
 	 * Private Constructor for the Singleton pattern.
 	 */
@@ -26,21 +28,28 @@ public class Game {
 	private HelloObserver helloObserver;
 	private GameState gameState;
 	private boolean isCurrentPlayer;
-
 	private String player;
+
 	/**
-	 * @param player the current (initial) player
+	 * This will be the new orientation at game state change or {@code null} if
+	 * the orientation did not change.
+	 */
+	private Orientation newOrientation;
+
+	/**
+	 * @param player
+	 *            the current (initial) player
 	 */
 	public void setPlayer(String player) {
 		this.player = player;
 	}
-	
-	//TODO this must make sure, that nothing remains from an old game,
+
+	// TODO this must make sure, that nothing remains from an old game,
 	// i.e. clean up old state
 	public void startGame() {
 		boolean isFirstPlayer = helloObserver.onGameStart(player);
-		if(isFirstPlayer){
-			
+		if (isFirstPlayer) {
+
 		}
 		isCurrentPlayer = !helloObserver.onGameStart(player);
 		if (isCurrentPlayer) {
@@ -81,26 +90,54 @@ public class Game {
 		gameUpdateObservers.remove(observer);
 	}
 
-	public void updateGameState(GameState state) {
+	/**
+	 * Updates the game state and notifies all observers. TODO Should be called
+	 * in intervals somewhere.
+	 */
+	private void updateGameState() {
 		// TODO
 		// this.gameState = state;
 		// notifyOnGameUpdate();
 	}
 
+	/**
+	 * Changes the orientation of the game to the left, if possible.
+	 */
 	public void leftPressed() {
-
+		if (gameState.getOrient() == Orientation.NORTH
+				|| gameState.getOrient() == Orientation.SOUTH) {
+			newOrientation = Orientation.EAST;
+		}
 	}
 
+	/**
+	 * Changes the orientation of the game to the right, if possible.
+	 */
 	public void rightPressed() {
-
+		if (gameState.getOrient() == Orientation.NORTH
+				|| gameState.getOrient() == Orientation.SOUTH) {
+			newOrientation = Orientation.WEST;
+		}
 	}
 
+	/**
+	 * Changes the orientation of the game to the up, if possible.
+	 */
 	public void upPressed() {
-
+		if (gameState.getOrient() == Orientation.WEST
+				|| gameState.getOrient() == Orientation.EAST) {
+			newOrientation = Orientation.NORTH;
+		}
 	}
 
+	/**
+	 * Changes the orientation of the game to the down, if possible.
+	 */
 	public void downPressed() {
-
+		if (gameState.getOrient() == Orientation.WEST
+				|| gameState.getOrient() == Orientation.EAST) {
+			newOrientation = Orientation.SOUTH;
+		}
 	}
 
 	public void onHello(String msgName) {
