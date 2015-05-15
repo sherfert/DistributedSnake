53592b95fd799d548f77b1758121b45bf44dd406
Satia Herfert <satiaherfert@gmx.de>
2015-05-15 23:03:48 +0200

    time values updated for better experience

--- test01/src/de/tud/tk3/distsnake/gameLogic/GameStateHelper.java
+++ test01/src/de/tud/tk3/distsnake/gameLogic/GameStateHelper.java
@@ -17,9 +17,9 @@ public class GameStateHelper {
 	
 	public static final int WIDTH = 30;
 	public static final int HEIGHT = 30;
-	public static final int DEFAULT_STEPS = 10;
+	public static final int DEFAULT_STEPS = 20;
 	
-	public static final int DEFAULT_STEP_TIME_MS = 1000;
+	public static final int DEFAULT_STEP_TIME_MS = 300;
 	
 	private static Random rand = new Random();
 
